7a7b112c700a1a12fabfb3f518b766ee0db562c7
pavank3673 <pavank3673@outlook.com>
2024-10-19 19:02:09 +0530

    1st task

--- BookPortal.js
+++ BookPortal.js
@@ -72,10 +72,96 @@ booklist[9] = {
   quantity: 10,
 };
 
+let cartlist = [];
+
+function showBooks() {
+  console.log(
+    "\nBook List:\n+---------+-----------------------+-------+------------+----------+\n| Book Id | Book Name             | Price | Status     | Quantity | \n+---------+-----------------------+-------+------------+----------+"
+  );
+  booklist.forEach((book, index) => {
+    console.log(
+      " " +
+        index +
+        "\t   " +
+        book.name +
+        "\t   " +
+        book.price +
+        "\t   " +
+        book.status +
+        "\t" +
+        book.quantity +
+        "\t"
+    );
+  });
+
+  menuDisplay();
+}
+
+function addBook() {
+  let bookId = question("Enter Book Id to add to cart : ");
+
+  let book = {};
+  book.book_name = booklist[bookId].name;
+  book.price = booklist[bookId].price;
+  book.quantity = 1;
+  book.total_price = book.price * book.quantity;
+
+  let cartlistBook = cartlist.find((existingBook) => {
+    return existingBook.book_name == book.book_name;
+  });
+
+  if (cartlistBook) {
+    let cartlistBookIndex = cartlist.indexOf(cartlistBook);
+    cartlist[cartlistBookIndex].quantity =
+      cartlist[cartlistBookIndex].quantity + 1;
+    cartlist[cartlistBookIndex].total_price =
+      cartlist[cartlistBookIndex].price * cartlist[cartlistBookIndex].quantity;
+  } else {
+    cartlist.push(book);
+  }
+
+  booklist[bookId].quantity = booklist[bookId].quantity - 1;
+
+  menuDisplay();
+}
+
+function showCart() {
+  console.log(
+    "\n Cart List :\n+-----------------------+-------+----------+-------------+\n| Book Name             | Price | Quantity | Total Price |\n+-----------------------+-------+----------+-------------+ "
+  );
+
+  cartlist.forEach((book) => {
+    console.log(
+      " " +
+        book.book_name +
+        "\t " +
+        book.price +
+        "\t " +
+        book.quantity +
+        "\t    " +
+        book.total_price +
+        "\t"
+    );
+  });
+
+  menuDisplay();
+}
 function menuDisplay() {
   let choice = question(
     "Display Menu :- \n 1) show available books to users \n 2) add book \n 3) show cart \n"
   );
+
+  switch (choice) {
+    case "1":
+      showBooks();
+      break;
+    case "2":
+      addBook();
+      break;
+    case "3":
+      showCart();
+      break;
+  }
 }
 
 menuDisplay();
